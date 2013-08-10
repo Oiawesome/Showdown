a51f6693a6c544a6e21f0cf57fb5e95e1db0f84c
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-10 11:31:05 -0700

    Update scripts.js

--- mods/sketchmons/scripts.js
+++ mods/sketchmons/scripts.js
@@ -2,7 +2,9 @@ exports.BattleScripts = {
         init: function() {
                 for (var i in this.data.Learnsets) {
                         var learnset = this.data.Learnsets[i].learnset;
-                            this.modData('Learnsets', i).learnset.sketch = ["5E"];
+                        var sk = learnset.sketch || [];
+                        if (!sk.length) {
+                            this.modData('Learnsets', i).learnset.sketch = ["5L0"];
                         }
                 }
         }
