1dd6c7e7584eebeabbb8a0d57dbaebb5b1d2e732
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-13 14:41:03 -0700

    add brave bird

--- mods/tutormons/scripts.js
+++ mods/tutormons/scripts.js
@@ -1,6 +1,39 @@
 exports.BattleScripts = {
 	init: function() {
 		// Brave Bird Tutor
-		this.modData('Learnsets', 'charizard').learnset.bravebird = ['5T'];
+	//Brave Bird Is now a tutor move
+	this.modData('Learnsets', 'charizard').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'butterfree').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'fearow').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'dodrio').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'gyarados').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'aerodactyl').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'articuno').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'zapdos').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'moltres').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'dragonite').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'noctowl').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'xatu').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'gligar').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'gliscor').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'lugia').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'peliper').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'masquerain').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'ninjask').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'salamence').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'rayquaza').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'chatot').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'togekiss').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'arceus').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'mew').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'pidove').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'tranquill').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'unfezant').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'archen').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'archeops').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'swanna').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'tornadus').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'thundurus').learnset.bravebird = ['5T'];
+	this.modData('Learnsets', 'landorus').learnset.bravebird = ['5T'];
 	}
 };
