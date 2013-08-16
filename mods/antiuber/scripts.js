baaae62270df54b70ff5950113085117838ec9cc
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-16 08:52:14 -0700

    place holders place holders and more place holders

--- mods/antiuber/scripts.js
+++ mods/antiuber/scripts.js
@@ -1,1 +1,11 @@
-//placeholder
+exports.BattleScripts = {
+	init: function() {
+		this.modData('Pokedex', 'cherrimsunshine').types = ['Grass', 'Fire'];
+
+		this.modData('Learnsets', 'masquerain').learnset.hurricane = ['5L100'];
+		this.modData('Learnsets', 'butterfree').learnset.hurricane = ['5L100'];
+		
+		this.modData('Pokedex', 'mismagius').abilities['1'] = 'Cursed Body';
+		this.modData('Pokedex', 'cryogonal').abilities['1'] = 'Ice Body';
+	}
+};
