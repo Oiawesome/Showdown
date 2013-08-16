f4ef1a363bc733e4bf3c7949b93d48a4fdd03936
Oesher Ayenew <Oiawesome99@yahoo.com>
2013-08-16 08:47:16 -0700

    Update scripts.js

--- mods/reversetiershift/scripts.js
+++ mods/reversetiershift/scripts.js
@@ -2,31 +2,28 @@ exports.BattleScripts = {
 	init: function() {
 		for (var i in this.data.Pokedex) {
 			var tier = '';
-			var adjustment = ;
+			var adjustment = 0;
 			if (this.data.FormatsData[i]) tier = this.data.FormatsData[i].tier;
 			switch (tier) {
+			case 'OU':
+				adjustment = -15;
+				break;				
 			case 'BL':
 			case 'UU':
-				adjustment = -5;
+				adjustment = -10;
 				break;
 			case 'BL2':
 			case 'RU':
-				adjustment = 10;
-				break;
-			case 'BL3':
-			case 'NU':
-			case 'NFE':
-			case 'LC':
-				adjustment = 15;
+				adjustment = -5;
 			}
 
 			if (adjustment) {
-				this.data.Pokedex[i].baseStats.hp += adjustment;
-				this.data.Pokedex[i].baseStats.atk += adjustment;
-				this.data.Pokedex[i].baseStats.def += adjustment;
-				this.data.Pokedex[i].baseStats.spa += adjustment;
-				this.data.Pokedex[i].baseStats.spd += adjustment;
-				this.data.Pokedex[i].baseStats.spe += adjustment;
+				this.data.Pokedex[i].baseStats.hp -= adjustment;
+				this.data.Pokedex[i].baseStats.atk -= adjustment;
+				this.data.Pokedex[i].baseStats.def -= adjustment;
+				this.data.Pokedex[i].baseStats.spa -= adjustment;
+				this.data.Pokedex[i].baseStats.spd -= adjustment;
+				this.data.Pokedex[i].baseStats.spe -= adjustment;
 			}
 		}
 	}
